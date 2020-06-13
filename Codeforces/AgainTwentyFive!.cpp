8844425868d9e6ffac3b3f932ccec3107ea02e0b
Dipankar Anand <myemailiddipankar@gmail.com>
2020-06-13 23:03:28 +0530

    Add files via upload

---
+++ Codeforces/AgainTwentyFive!.cpp
@@ -0,0 +1,13 @@
+/*
+Question Link: https://codeforces.com/problemset/problem/630/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   long long int t;
+   cin>>t;
+   cout<<25<<endl;
+}
