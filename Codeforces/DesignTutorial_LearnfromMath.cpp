339f6d1dbc06e31c2c3ec3047d29d1ec1521d75a
Dipankar Anand <myemailiddipankar@gmail.com>
2020-07-17 22:08:48 +0530

    Add files via upload

---
+++ Codeforces/DesignTutorial_LearnfromMath.cpp
@@ -0,0 +1,17 @@
+/*
+Question Link: https://codeforces.com/problemset/problem/472/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   if(n%2==0){
+       cout<<4<<" "<<(n-4)<<endl;
+   }else{
+       cout<<9<<" "<<(n-9)<<endl;
+   }
+}
