4625b308e39d8285cd88273a38f0c471a6ec1097
Dipankar Anand <myemailiddipankar@gmail.com>
2020-09-01 22:01:22 +0530

    Add files via upload

---
+++ Codeforces/OmkarandCompletion.cpp
@@ -0,0 +1,26 @@
+/*
+Question Link: https://codeforces.com/problemset/problem/1372/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define vec vector<int>
+
+void solve(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"1 ";
+    }cout<<endl;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
